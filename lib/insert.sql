d5ab906e27e4a5aa8c2448db0f601312d422f149
goku <wkurts247@gmail.com>
2023-08-27 09:05:50 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate", 20, "M", "Red","Harsh",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 26, "M", "Black","Polite",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 18, "F", "Green","Loving",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 45, "M", "Orange","Harsh",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa", 35, "F", "Blue","Polite",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 31, "M", "Red","Harsh",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 26, "F", "Pink","Angry",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 62, "M", "Yellow","Calm",0);
